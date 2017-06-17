083f5c82377f01a1afa141d842b5af4e7c272631
B Kleinen <kleinen@infrastructure.de>
2017-06-17 23:46:38 +0200

    longest trip for route

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,10 @@ class Route < ApplicationRecord
   def trips
     Trip.where(route_identifier: route_identifier)
   end
-  
+  def longest_trip
+    length_trip_id = trips.collect {|t| [t.length, t.id]}
+    max = trips.maximum("length_cache")
+    longest_id = length_trip_id.select{|p| p.first == max}.first.last
+    return Trip.find(longest_id)
+  end
 end
